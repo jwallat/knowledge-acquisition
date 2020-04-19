4036f130493b9eef8bcf706e1d7fdf3f42584b3f
Jonas <jwallat@hotmail.de>
2020-04-19 15:58:06 +0200

    Allow for ordering and clustering on plots

--- knowledge_probing/plotting/layer_wise_plots.py
+++ knowledge_probing/plotting/layer_wise_plots.py
@@ -4,12 +4,20 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 from tqdm import tqdm
+import copy
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.cluster import KMeans
 
 
-def produce_layer_wise_plots():
-    with open('data/layer_data/classifier_per_layer/1/default_data.json') as json_data:
+def produce_layer_wise_plots(orderType):
+
+    data_dir = 'data/layer_data/classifier_per_layer/long_e100_p15_d0_01/new_decoder/'
+
+    with open('{}1/default_data.json'.format(data_dir)) as json_data:
         default_data_1 = json.load(json_data)
         # print(default_data)
+
+    clustering_data = []
 
     for dataset in ['Google_RE', 'TREx', 'ConceptNet', 'Squad']:
         for relation in tqdm(default_data_1[dataset].keys()):
@@ -25,10 +33,10 @@ def produce_layer_wise_plots():
 
                     # Load data from files
 
-                    qa_file = 'data/layer_data/classifier_per_layer/{}/qa_data.json'.format(
-                        layer + 1)
-                    default_file = 'data/layer_data/classifier_per_layer/{}/default_data.json'.format(
-                        layer + 1)
+                    qa_file = '{}{}/qa_data.json'.format(
+                        data_dir, layer + 1)
+                    default_file = '{}{}/default_data.json'.format(
+                        data_dir, layer + 1)
 
                     if os.path.isfile(qa_file):
                         with open(qa_file) as json_data:
@@ -48,6 +56,7 @@ def produce_layer_wise_plots():
                     else:
                         default_vals.append(-1)
 
+                # Plotting data
                 wide_df = pd.DataFrame(
                     dict(Layer=layers, Default=default_vals, QA=qa_vals))
                 tidy_df = wide_df.melt(id_vars='Layer')
@@ -61,8 +70,159 @@ def produce_layer_wise_plots():
                     yaxis_title=metric,
                 )
 
-                if not os.path.exists("data/layer_data/classifier_per_layer/plots/png/"):
-                    os.mkdir("data/layer_data/classifier_per_layer/plots/png/")
+                if orderType == 'None':
+                    if not os.path.exists("{}plots/png/".format(data_dir)):
+                        os.mkdir("{}plots/png/".format(data_dir))
 
-                fig.write_image(
-                    "data/layer_data/classifier_per_layer/plots/png/{}_{}_{}.png".format(dataset, relation, metric))
+                    fig.write_image(
+                        "{}plots/png/{}_{}_{}.png".format(data_dir, dataset, relation, metric))
+
+                elif orderType == 'naive':
+
+                    # Getting an order on this data
+                    diff_layer = find_layer_differences(qa_vals, default_vals)
+
+                    if diff_layer == -1:
+                        ordering = -1
+                    elif diff_layer >= 1 and diff_layer <= 3:
+                        ordering = 'early'
+                    elif diff_layer >= 4 and diff_layer <= 9:
+                        ordering = 'middle'
+                    elif diff_layer >= 10 and diff_layer <= 12:
+                        ordering = 'late'
+                    else:
+                        ordering = 'Unknown-ordering'
+
+                    if not os.path.exists("{}plots/png_naive_ordering/".format(data_dir)):
+                        os.mkdir("{}plots/png_naive_ordering/".format(data_dir))
+
+                    fig.write_image(
+                        "{}plots/png_naive_ordering/{}-layer_{}_{}_{}.png".format(data_dir, ordering, dataset, relation, metric))
+
+                elif orderType == 'clustering':
+
+                    data_point = {
+                        'name': '{}_{}_{}'.format(dataset, relation, metric),
+                        'default_vals': default_vals,
+                        'qa_vals': qa_vals
+                    }
+
+                    for layer in range(12):
+                        data_point['diff_{}'.format(
+                            layer + 1)] = abs(default_vals[layer] - qa_vals[layer])
+                        # Curve going down or up?
+                        if layer > 0:
+                            if default_vals[layer] - default_vals[layer - 1] == 0:
+                                data_point['curve_default_{}'.format(
+                                    layer + 1)] = 0
+                            elif default_vals[layer] - default_vals[layer - 1] > 0:
+                                data_point['curve_default_{}'.format(
+                                    layer + 1)] = 1
+                            else:
+                                data_point['curve_default_{}'.format(
+                                    layer + 1)] = -1
+
+                            if qa_vals[layer] - qa_vals[layer - 1] == 0:
+                                data_point['curve_qa_{}'.format(layer + 1)] = 0
+                            elif qa_vals[layer] - qa_vals[layer - 1] > 0:
+                                data_point['curve_qa_{}'.format(layer + 1)] = 1
+                            else:
+                                data_point['curve_qa_{}'.format(
+                                    layer + 1)] = -1
+                        else:
+                            data_point['curve_default_{}'.format(
+                                layer + 1)] = 1 if default_vals[layer] > 0 else 0
+                            data_point['curve_qa_{}'.format(
+                                layer + 1)] = 1 if qa_vals[layer] > 0 else 0
+                    clustering_data.append(data_point)
+
+                else:
+                    print('Unknown order type')
+
+    if orderType == 'clustering':
+        num_clusters = 8
+
+        print(clustering_data[0])
+
+        cluster_assignments = do_clustered_plots(
+            data_dir, clustering_data, num_clusters)
+
+        print(clustering_data[0])
+
+        if not os.path.exists("{}plots/png_clustered/".format(data_dir)):
+            os.mkdir("{}plots/png_clustered/".format(data_dir))
+
+        for i, ele in enumerate(clustering_data):
+
+            qa_vals = ele['qa_vals']
+            default_vals = ele['default_vals']
+
+            # Plotting data
+            wide_df = pd.DataFrame(
+                dict(Layer=layers, Default=default_vals, QA=qa_vals))
+            tidy_df = wide_df.melt(id_vars='Layer')
+
+            # Plot
+            fig = px.line(tidy_df, x='Layer', y='value', color='variable',
+                          title='{} {} [{}]'.format(dataset, relation, metric), )
+
+            fig.update_layout(
+                xaxis_title="Layer",
+                yaxis_title=metric,
+            )
+
+            cluster = cluster_assignments[i]
+
+            fig.write_image(
+                "{}plots/png_clustered/{}-cluster_{}.png".format(data_dir, cluster, ele['name']))
+
+
+def find_layer_differences(qa_vals, default_vals):
+    ''' 
+    This function is supposed to retun the layer in which the difference between qa val and default 
+    val gets bigger than the mean difference on all layers.
+    '''
+    mean_difference = get_mean_difference(default_vals, qa_vals)
+
+    for layer in range(12):
+        difference = abs(default_vals[layer] - qa_vals[layer])
+
+        if difference > mean_difference:
+            return layer + 1
+
+    return -1
+
+
+def get_mean_difference(default, qa):
+    sum_differences = 0
+    for layer in range(12):
+        sum_differences = sum_differences + abs(default[layer] - qa[layer])
+
+    mean_difference = sum_differences / len(default)
+    return mean_difference
+
+
+def do_clustered_plots(data_dir, cluster_data, num_clusters):
+    print('Clustering....')
+
+    data = copy.deepcopy(cluster_data)
+
+    for ele in data:
+        ele.pop('name', None)
+        ele.pop('qa_vals', None)
+        ele.pop('default_vals', None)
+
+    vec = DictVectorizer()
+
+    transformed = vec.fit_transform(data).toarray()
+
+    # KMeans
+    kmeans = KMeans(n_clusters=num_clusters)
+
+    # fit
+    kmeans.fit(transformed)
+
+    # Predict
+    out = kmeans.fit_predict(transformed)
+
+    return out
--- produce_layerwise_plots.py
+++ produce_layerwise_plots.py
@@ -1,9 +1,18 @@
 from knowledge_probing.plotting.layer_wise_plots import produce_layer_wise_plots
+from argparse import ArgumentParser
 
 
-def main():
-    produce_layer_wise_plots()
+def main(args):
+    print('Using order type: {}'.format(args.order_type))
+    produce_layer_wise_plots(args.order_type)
 
 
 if __name__ == '__main__':
-    main()
+    parser = ArgumentParser()
+
+    parser.add_argument('--order_type', default='None',
+                        choices=['None', 'naive', 'clustering'])
+
+    args = parser.parse_args()
+
+    main(args)
