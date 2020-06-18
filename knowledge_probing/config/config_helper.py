9b4de475e9d5073306f2bb26e71bec0c748f4ffa
Jonas <jwallat@hotmail.de>
2020-06-18 19:18:34 +0200

    Reworked the configs to only use argparse This means using yaml files for additional config is retired

--- knowledge_probing/config/config_helper.py
+++ knowledge_probing/config/config_helper.py
@@ -6,61 +6,29 @@ import os
 
 
 def handle_config(args):
-    # Load the config:
-    config = load_config(args.config_path)
-    args = DotMap(vars(args))
-
-    args = add_config_to_args(config, args)
-
-    # The idea is to load the config and set all necessary implications (e.g. lowercase True/False from bert_model_type)
+    # The idea is to handle all necessary implications (e.g. lowercase True/False from bert_model_type)
     args.device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
+
     args.run_identifier = build_run_identifier(args)
 
-    # Data dir
-    assert os.path.exists(args.data_dir)
+    assert os.path.exists(args.probing_data_dir)
 
-    # Output dir
     args.output_dir = '{}{}'.format(args.output_base_dir, args.run_identifier)
+    print(args.output_dir)
+    os.makedirs(args.output_dir)
 
-    # Model dirs
-    args.decoder_save_dir = '{}/decoder/{}'.format(
-        args.output_dir, args.bert_type)
-
-    # Execution log
-    args.execution_log = args.output_dir + '/execution_log.txt'
-
-    os.makedirs(args.output_dir, exist_ok=True)
-    # os.makedirs(args.pre_trained_model_dir, exist_ok=True)
-    os.makedirs(args.qa_model_dir, exist_ok=True)
+    args.decoder_save_dir = '{}/decoder/'.format(
+        args.output_dir)
     os.makedirs(args.decoder_save_dir, exist_ok=True)
 
-    # Lowercase
+    args.execution_log = args.output_dir + '/execution_log.txt'
+
+    if args.use_model_from_dir:
+        assert os.path.exists(args.model_dir)
+
     args.lowercase = False
     if 'uncased' in args.bert_model_type:
         args.lowercase = True
-
-    # Choose the correct training dataset
-    if args.do_training:
-        if args.training_dataset == 'wikitext-2':
-            args.train_data_file = args.wiki2_train_data_file
-            args.eval_data_file = args.wiki2_eval_data_file
-            args.test_data_file = args.wiki2_test_data_file
-        elif args.training_dataset == 'wikitext-103':
-            args.train_data_file = args.wiki103_train_data_file
-            args.eval_data_file = args.wiki103_eval_data_file
-            args.test_data_file = args.wiki103_test_data_file
-        else:
-            print('If you can read this, you have probabily added a new training dataset. Please make sure to add the path to the dataset files in the config.yaml and the config_helper.py')
-
-    # Fix the type of learning_rate
-    args.learning_rate = float(args.learning_rate)
-
-    return args
-
-
-def add_config_to_args(config, args):
-    for key in config:
-        args[key] = config[key]
 
     return args
 
@@ -73,8 +41,8 @@ def build_run_identifier(args):
 
     model_type_postfix = args.bert_model_type.split('-')[-1]
 
-    run_identifier = '{}_{}_{}_trained-{}_{}_{}'.format(
-        args.run_name, args.bert_type, model_type_postfix, args.do_training, args.probing_model, timestamp)
+    run_identifier = '{}_{}_trained-{}_{}_{}'.format(
+        args.run_name, model_type_postfix, args.do_training, args.decoder_type, timestamp)
 
     print('Run identifier: ', run_identifier)
     return run_identifier
