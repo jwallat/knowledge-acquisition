5f4c65f06d92282d7680c9252ac0924052d304b5
ztywxy <52611192+ztywxy@users.noreply.github.com>
2021-11-21 17:59:08 +0100

    Delete scripts/plots directory

--- scripts/plots/produce_layerwise_comparison_plots.py
+++
@@ -1,9 +0,0 @@
-from knowledge_probing.plotting.layer_wise_comparison_plots import produce_layer_wise_comparison_plots
-
-
-def main():
-    produce_layer_wise_comparison_plots()
-
-
-if __name__ == '__main__':
-    main()
